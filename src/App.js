7dccf1a1cff312508ed6571b58029958858ef391
basharAbdullah <des.basharabdulah@gmail.com>
2023-04-30 13:18:58 +0300

    Track visitor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import logo from "./Routes_logo.e4586c14.svg";
+import { useVisitorData } from "@fingerprintjs/fingerprintjs-pro-react";
 
 const strings = {
   phoneNumber: {
@@ -24,6 +25,8 @@ function App() {
   const [fullName, setFullName] = useState();
   const [code, setCode] = useState();
   const [phoneNumber, setPhoneNumber] = useState();
+
+  const { isLoading, error, data } = useVisitorData();
 
   // Get unique id using browser local storage
   // function getMachineId() {
@@ -60,128 +63,147 @@ function App() {
     }, 2000);
   };
 
-  return (
-    <div className="App">
-      <header className="row">
-        <div className="logo col-6">
-          <img
-            alt="logo"
-            src="https://storage.googleapis.com/wzukusers/user-34680569/images/5d89cff004d96YVt5GU3/Header-Logo-01_d200.png"
-          />
-        </div>
-        <div className="nav-items col-6">
-          <ul>
-            {/* <li>About us</li>
-            <li>Contact</li> */}
-            <li>Phone number verification</li>
-          </ul>
-        </div>
-      </header>
-      <main id="content">
-        <div className="row">
-          <div className="middle-logo">
-            <img height={20} src={logo} />
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
+  }
+
+  if (data) {
+    // perform some logic based on the visitor data
+    return (
+      <div className="App">
+        <header className="row">
+          <div className="logo col-6">
+            <img
+              alt="logo"
+              src="https://storage.googleapis.com/wzukusers/user-34680569/images/5d89cff004d96YVt5GU3/Header-Logo-01_d200.png"
+            />
           </div>
-          <div className="col-12">
-            <div className="container form-container">
-              <form onSubmit={handleSubmit}>
-                <h3>
-                  {codeMode ? strings.code.title : strings.phoneNumber.title}
-                </h3>
-                <div className="row">
-                  <div className="col">
-                    <label>
-                      {codeMode
-                        ? strings.code.label
-                        : strings.phoneNumber.label}
-                    </label>
-                    <input
-                      name="phoneNumber"
-                      required
-                      type="text"
-                      class="form-control"
-                      value={phoneNumber}
-                      onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
+          <div className="nav-items col-6">
+            <ul>
+              {/* <li>About us</li>
+                <li>Contact</li> */}
+              <li>Phone number verification</li>
+            </ul>
+          </div>
+        </header>
+        <main id="content">
+          <div className="row">
+            <div className="middle-logo">
+              <img height={20} src={logo} />
+            </div>
+            <div className="col-12">
+              <div className="container form-container">
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    Welcome {data.visitorFound ? `back ${data.visitorId}` : ""}!
                   </div>
-                </div>
-                {codeMode ? (
-                  ""
-                ) : (
+                  {/* <h3>
+                    {codeMode ? strings.code.title : strings.phoneNumber.title}
+                  </h3>
+                  <h5>
+                   Your device id is:{" "} 
+                  <code>{generateUniqueID().replace(/\s+/g, "")}</code> 
+                  </h5>
                   <div className="row">
                     <div className="col">
-                      <label>Full Name</label>
+                      <label>
+                        {codeMode
+                          ? strings.code.label
+                          : strings.phoneNumber.label}
+                      </label>
                       <input
+                        name="phoneNumber"
                         required
-                        name="fullName"
                         type="text"
                         class="form-control"
-                        value={fullName}
-                        onChange={(e) => setFullName(e.target.value)}
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
                       />
                     </div>
                   </div>
-                )}
-                <div className="row">
-                  <div className="col">
-                    <button
-                      // onClick={handleSubmit}
-                      className={`d-flex justify-content-center align-items-center ${
-                        codeMode ? "code" : "number"
-                      }`}
-                    >
-                      {lodaing ? (
-                        <div
-                          style={{ width: "25px", height: "25px" }}
-                          class="spinner-border text-dark"
-                          role="status"
-                        ></div>
-                      ) : codeMode ? (
-                        strings.code.button
-                      ) : (
-                        strings.phoneNumber.button
-                      )}
-                    </button>
-                  </div>
-                </div>
-              </form>
+                  {codeMode ? (
+                    ""
+                  ) : (
+                    <div className="row">
+                      <div className="col">
+                        <label>Full Name</label>
+                        <input
+                          required
+                          name="fullName"
+                          type="text"
+                          class="form-control"
+                          value={fullName}
+                          onChange={(e) => setFullName(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                  )}
+                  <div className="row">
+                    <div className="col">
+                      <button
+                        // onClick={handleSubmit}
+                        className={`d-flex justify-content-center align-items-center ${
+                          codeMode ? "code" : "number"
+                        }`}
+                      >
+                        {lodaing ? (
+                          <div
+                            style={{ width: "25px", height: "25px" }}
+                            class="spinner-border text-dark"
+                            role="status"
+                          ></div>
+                        ) : codeMode ? (
+                          strings.code.button
+                        ) : (
+                          strings.phoneNumber.button
+                        )}
+                      </button>
+                    </div>
+                  </div> */}
+                </form>
+              </div>
             </div>
           </div>
+        </main>
+        <div className="row footer-row">
+          <footer class="d-flex  flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div className="container d-flex align-items-center justify-content-between">
+              <div class="col-md-4 d-flex align-items-center copyright">
+                <img height={20} src={logo} />
+
+                <span class="m ml-1 text-muted">
+                  &copy; 2023, All rights reserved
+                </span>
+              </div>
+
+              <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li className="ms-3">
+                  <a className="text-muted" href="#">
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                </li>
+                <li className="ms-3">
+                  <a className="text-muted" href="#">
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                </li>
+                <li className="ms-3">
+                  <a className="text-muted" href="#">
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </footer>
         </div>
-      </main>
-      <div className="row footer-row">
-        <footer class="d-flex  flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div className="container d-flex align-items-center justify-content-between">
-            <div class="col-md-4 d-flex align-items-center copyright">
-              <img height={20} src={logo} />
-
-              <span class="m ml-1 text-muted">
-                &copy; 2023, All rights reserved
-              </span>
-            </div>
-
-            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-              <li className="ms-3">
-                <a className="text-muted" href="#">
-                  <i className="bi bi-instagram"></i>
-                </a>
-              </li>
-              <li className="ms-3">
-                <a className="text-muted" href="#">
-                  <i className="bi bi-facebook"></i>
-                </a>
-              </li>
-              <li className="ms-3">
-                <a className="text-muted" href="#">
-                  <i className="bi bi-twitter"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FpjsProvider
+      loadOptions={{
+        apiKey: "Ih9Z8CbyijXdZje4g1LT",
+      }}
+    >
+      <App />
+    </FpjsProvider>
   </React.StrictMode>
 );
